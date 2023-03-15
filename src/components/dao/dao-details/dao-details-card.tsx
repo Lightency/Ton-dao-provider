cbbb882a6f962e7c8842be31fc1d6f54547e1671
takwa <takouaguetat@gmail.com>
2023-03-15 09:24:18 +0100

    NftDetails

---
+++ src/components/dao/dao-details/dao-details-card.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import cn from 'classnames';
+import Button from '@/components/ui/button';
+
+// FIXME: need to add vote type
+export default function DaoDetailsCard({ dao }: any) {
+  let [isExpand, setIsExpand] = useState(false);
+  return (
+    <motion.div
+      layout
+      initial={{ borderRadius: 8 }}
+      className={cn(
+        'mb-3 rounded-lg bg-white p-5 transition-shadow duration-200 dark:bg-light-dark xs:p-6',
+        isExpand ? 'shadow-large' : 'shadow-card hover:shadow-large'
+      )}
+    >
+      <motion.div
+        layout
+        className="flex w-full flex-col-reverse justify-between md:grid md:grid-cols-3"
+      >
+        <div className="self-start md:col-span-2">
+          <h3
+            onClick={() => setIsExpand(!isExpand)}
+            className="cursor-pointer text-base font-medium leading-normal dark:text-gray-100 2xl:text-lg"
+          >
+            {dao.title}
+          </h3>
+          <p className="mt-2 text-gray-600 dark:text-gray-400">Dao #{dao.id}</p>
+          <h4 className="mt-2 mb-6 uppercase dark:text-gray-100">
+            Description
+          </h4>
+
+          <div
+            className="dynamic-html grid gap-2 leading-relaxed text-gray-600 dark:text-gray-400"
+            dangerouslySetInnerHTML={{ __html: dao.description }}
+          />
+          <Button
+            onClick={() => setIsExpand(!isExpand)}
+            className="mt-4 w-full xs:mt-6 xs:w-auto md:mt-10"
+            shape="rounded"
+          >
+            Details
+          </Button>
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+}
---
+++ src/data/static/dao-data.tsx
@@ -0,0 +1,38 @@
+export const daos = [
+  {
+    id: '1',
+    title: 'First DAO',
+    description: `
+          <h6>Simple Summary</h6>
+          <p>Extend POOL drip for UNI, COMP, Dai, USDC, and SUSHI Ethereum pools until the end of October. Extend prizes and drip for the POOL pool as well.</p>
+          <h6>Motivation</h6>
+          <p>The POOL drips for most Ethereum pools will be ending within the next 2 weeks (see details here). This proposal serves to maintain the statusquo of POOL incentives on Ethereum. However, it only extends the status quo for a relatively short amount of additional time. This is becausethe V4 prize pool will be launching in mid October and a larger incentive overhaul will be.</p>
+          <h6>Where Can I get Some?</h6>
+          <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+        `,
+  },
+  {
+    id: '2',
+    title: 'Second DAO',
+    description: `
+          <h6>Simple Summary</h6>
+          <p>Extend POOL drip for UNI, COMP, Dai, USDC, and SUSHI Ethereum pools until the end of October. Extend prizes and drip for the POOL pool as well.</p>
+          <h6>Motivation</h6>
+          <p>The POOL drips for most Ethereum pools will be ending within the next 2 weeks (see details here). This proposal serves to maintain the statusquo of POOL incentives on Ethereum. However, it only extends the status quo for a relatively short amount of additional time. This is becausethe V4 prize pool will be launching in mid October and a larger incentive overhaul will be.</p>
+          <h6>Where Can I get Some?</h6>
+          <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+        `,
+  },
+  {
+    id: '3',
+    title: 'Third DAO',
+    description: `
+          <h6>Simple Summary</h6>
+          <p>Extend POOL drip for UNI, COMP, Dai, USDC, and SUSHI Ethereum pools until the end of October. Extend prizes and drip for the POOL pool as well.</p>
+          <h6>Motivation</h6>
+          <p>The POOL drips for most Ethereum pools will be ending within the next 2 weeks (see details here). This proposal serves to maintain the statusquo of POOL incentives on Ethereum. However, it only extends the status quo for a relatively short amount of additional time. This is becausethe V4 prize pool will be launching in mid October and a larger incentive overhaul will be.</p>
+          <h6>Where Can I get Some?</h6>
+          <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+        `,
+  },
+];
